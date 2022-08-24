b97976412ef8ceb158cfa275ef0ef6e112aabe10
Venki <ganjivenkateswarlu@gmail.com>
2022-08-24 21:21:23 +0530

    refactor: add semicolons

--- index.js
+++ index.js
@@ -1,53 +1,53 @@
-var express = require('express')
-var fs = require('fs')
-var multer = require('multer')
-var path = require('path')
+var express = require('express');
+var fs = require('fs');
+var multer = require('multer');
+var path = require('path');
 
-var app = express()
-var upload = multer({ dest: 'files/' })
+var app = express();
+var upload = multer({ dest: 'files/' });
 
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'))
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/get-request', function (req, res) {
   // Remove "world" from the below line
-  res.locals.name = req.query.name || 'world'
-  res.render('index')
-})
+  res.locals.name = req.query.name || 'world';
+  res.render('index');
+});
 
 app.get('/get-json', function (req, res) {
   // Change "files/sample.json" to "files/sample.zip" in the below line
-  var file = path.join(__dirname, 'files/sample.json')
+  var file = path.join(__dirname, 'files/sample.json');
   fs.readFile(file, 'utf8', function (err, data) {
     if (err) {
-      res.status(500).send(err.message || err.toString())
+      res.status(500).send(err.message || err.toString());
     } else {
-      res.send(JSON.parse(data))
+      res.send(JSON.parse(data));
     }
-  })
-})
+  });
+});
 
 app.post('/post-request', function (req, res) {
   // Make the request body an array of two numbers
-  var sum = req.body.input[0] * req.body.input[1]
-  res.send(sum.toString())
-})
+  var sum = req.body.input[0] * req.body.input[1];
+  res.send(sum.toString());
+});
 
 app.post('/upload-file', upload.single('photo'), function (req, res) {
   // Set file name as "picture" in the request body
-  res.send(req.file.filename)
-})
+  res.send(req.file.filename);
+});
 
 app.get('/download-file', function (req, res) {
   // Change "files/sample.zip" to "sample.zip" in the below line
-  var file = path.join(__dirname, 'files/sample.zip')
-  res.download(file)
-})
+  var file = path.join(__dirname, 'files/sample.zip');
+  res.download(file);
+});
 
 app.all('*', function (req, res) {
-  res.send({})
-})
+  res.send({});
+});
 
-app.listen(14482)
+app.listen(14482);
--- server.js
+++ server.js
@@ -1,58 +1,58 @@
 /**
  * Put this Errsole code snippet at the top of your app's main file
  */
-const errsole = require('errsole')
+const errsole = require('errsole');
 errsole.initialize({
   framework: 'express',
   token: '9d596fe4-6ea0-48da-908b-caf47f70aa6c'
-})
+});
 // End of Errsole code snippet
 
-var express = require('express')
-var fs = require('fs')
-var multer = require('multer')
-var path = require('path')
+var express = require('express');
+var fs = require('fs');
+var multer = require('multer');
+var path = require('path');
 
-var app = express()
-var upload = multer({ dest: 'files/' })
+var app = express();
+var upload = multer({ dest: 'files/' });
 
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'))
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/get-request', function (req, res) {
-  res.locals.name = req.query.name
-  res.render('index')
-})
+  res.locals.name = req.query.name;
+  res.render('index');
+});
 
 app.get('/get-json', function (req, res) {
-  var file = path.join(__dirname, 'files/sample.zip')
+  var file = path.join(__dirname, 'files/sample.zip');
   fs.readFile(file, 'utf8', function (err, data) {
     if (err) {
-      res.status(500).send(err.message || err.toString())
+      res.status(500).send(err.message || err.toString());
     } else {
-      res.send(JSON.parse(data))
+      res.send(JSON.parse(data));
     }
-  })
-})
+  });
+});
 
 app.post('/post-request', function (req, res) {
-  var result = req.body.input[0] * req.body.input[1]
-  res.send(result.toString())
-})
+  var result = req.body.input[0] * req.body.input[1];
+  res.send(result.toString());
+});
 
 app.post('/upload-file', upload.single('photo'), function (req, res) {
-  res.send(req.file.filename)
-})
+  res.send(req.file.filename);
+});
 
 app.get('/download-file', function (req, res) {
-  var file = path.join(__dirname, 'sample.zip')
-  res.download(file)
-})
+  var file = path.join(__dirname, 'sample.zip');
+  res.download(file);
+});
 
 app.all('*', function (req, res) {
-  res.send({})
-})
+  res.send({});
+});
 
-app.listen(14874)
+app.listen(14874);
